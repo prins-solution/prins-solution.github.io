ac64a4077dd6517e7cf66cd7340c6f91380fa666
shincyls <shincy.mac@gmail.com>
2020-04-22 23:18:03 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,10 +58,10 @@
   // Mobile Navigation
   if ($('.nav-menu').length) {
     var $mobile_nav = $('.nav-menu').clone().prop({
-      class: 'mobile-nav d-lg-none'
+      class: 'mobile-nav'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="bx bx-menu"></i></button>');
+    $('body').prepend('</span><a class="mobile-nav-toggle"><b>MENU</b></a>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
